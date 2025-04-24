a630e6145fb6096c51dac2106cd587b74a14ada2
priti <priti12bishnoi@gmail.com>
2025-04-24 12:03:52 +0530

    portfolio using react js and module css

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={ styles.container }>
+      <div className={ styles.content }>
+        <h1 className={ styles.title }>Hi, I'm Priti</h1>
+        <p className={ styles.description }>
+          I'm a full-stack developer with 1 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <div >
+          <a href="mailto:priti12bishnoi@gmail.com" className={ styles.contactBtn }>
+            Contact Me
+          </a>
+          <a href="assets/Priti-Bishnoi-CV.pdf" download class={ styles.contactBtn }>Download CV</a>
+        </div>
+      </div>
+      <img
+        src={ getImageUrl("hero/heroImage.png") }
+        alt="Hero image of me"
+        className={ styles.heroImg }
+      />
+      <div className={ styles.topBlur } />
+      <div className={ styles.bottomBlur } />
+    </section>
+  );
+};
